07d3fc2952344e6c33940ea9a7b5017ec237950e
Stefan Hoehn <webmaster@untergang.de>
2023-07-02 22:02:51 +0200

    Shellcheck, wieder mal da Quoten vergessen . Dammit !

--- wetter10.sh
+++ wetter10.sh
@@ -3,10 +3,10 @@
 anfang=$(date -d "next hour" '+%Y-%m-%dT%H')
 ende=$(date -d "next week" '+%Y-%m-%dT%H')
 
-wetterdienst values --provider=dwd --network=mosmix --resolution=large --parameter=TTT --station=K2635 --date=$anfang/$ende | jq -r '.[]| [.date,.value -273.15 ] | @csv'>temp.csv
-wetterdienst values --provider=dwd --network=mosmix --resolution=large --parameter=E_TTT --station=K2635 --date=$anfang/$ende | jq -r '.[]|[.date,.value ] | @csv'>tempe.csv
-wetterdienst values --provider=dwd --network=mosmix --resolution=large --parameter=R101 --station=K2635 --date=$anfang/$ende | jq -r '.[]| [.date,.value ] | @csv'>regen1.csv
-wetterdienst values --provider=dwd --network=mosmix --resolution=large --parameter=R110 --station=K2635 --date=$anfang/$ende | jq -r '.[]| [.date,.value ] | @csv'>regen10.csv
+wetterdienst values --provider=dwd --network=mosmix --resolution=large --parameter=TTT --station=K2635 --date="$anfang"/"$ende" | jq -r '.[]| [.date,.value -273.15 ] | @csv'>temp.csv
+wetterdienst values --provider=dwd --network=mosmix --resolution=large --parameter=E_TTT --station=K2635 --date="$anfang"/"$ende" | jq -r '.[]|[.date,.value ] | @csv'>tempe.csv
+wetterdienst values --provider=dwd --network=mosmix --resolution=large --parameter=R101 --station=K2635 --date="$anfang"/"$ende" | jq -r '.[]| [.date,.value ] | @csv'>regen1.csv
+wetterdienst values --provider=dwd --network=mosmix --resolution=large --parameter=R110 --station=K2635 --date="$anfang"/"$ende" | jq -r '.[]| [.date,.value ] | @csv'>regen10.csv
 
 paste -d ',' temp.csv regen1.csv regen10.csv tempe.csv > wetter.csv
 

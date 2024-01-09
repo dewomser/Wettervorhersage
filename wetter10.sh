7b50f0a083f13c4d497769bffb9426bd9573e8f6
Stefan Hoehn <webmaster@untergang.de>
2024-01-09 10:11:19 +0100

    Pfad zu Wettervorhersagewird erkannt Station ist eine Variable Shellcheck-zeugs  Quotierung

--- wetter10.sh
+++ wetter10.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
-
-anfang=$(date -d "next hour" '+%Y-%m-%dT%H')
-ende=$(date -d "next week" '+%Y-%m-%dT%H')
+# wetterd="/home/karl/.local/bin/wetterdienst"
+wetterd="$(which wetterdienst)"
+station="K2635"
+cd /home/foo/bin/wetter||exit
+anfang="$(date -d "next hour" '+%Y-%m-%dT%H')"
+ende="$(date -d "next week" '+%Y-%m-%dT%H')"
 
 ##wetterdienst values --provider=dwd --network=mosmix --resolution=large --parameter=TTT --station=K2635 --date=$anfang/$ende | jq -r '.[]| [.date,.value -273.15 ] | @csv'>temp.csv
-wetterdienst values --provider=dwd --network=mosmix --resolution=large --parameter=TTT --station=K2635 --date="$anfang"/"$ende" --format=csv>temp.csv
-wetterdienst values --provider=dwd --network=mosmix --resolution=large --parameter=E_TTT --station=K2635 --date="$anfang"/"$ende" --format=csv>tempe.csv
-wetterdienst values --provider=dwd --network=mosmix --resolution=large --parameter=R101 --station=K2635 --date="$anfang"/"$ende" --format=csv>regen1.csv
-wetterdienst values --provider=dwd --network=mosmix --resolution=large --parameter=R110 --station=K2635 --date="$anfang"/"$ende" --format=csv>regen10.csv
+$wetterd values --provider=dwd --network=mosmix --resolution=large --parameter=TTT --station="$station" --date="$anfang"/"$ende" --format=csv>temp.csv
+$wetterd values --provider=dwd --network=mosmix --resolution=large --parameter=E_TTT --station="$station" --date="$anfang"/"$ende" --format=csv>tempe.csv
+$wetterd values --provider=dwd --network=mosmix --resolution=large --parameter=R101 --station="$station" --date="$anfang"/"$ende" --format=csv>regen1.csv
+$wetterd values --provider=dwd --network=mosmix --resolution=large --parameter=R110 --station="$station" --date="$anfang"/"$ende" --format=csv>regen10.csv
 paste -d ',' temp.csv regen1.csv regen10.csv tempe.csv > wetter.csv
 
 sleep 2
 
-gnuplot wetter1.gp
+gnuplot wetter2.gp
 
 sleep 2
 
